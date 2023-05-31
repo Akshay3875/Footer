5f3093132a69394e14f7fe399cee19b7f6d13654
Akshay3875 <121480219+Akshay3875@users.noreply.github.com>
2023-05-31 14:03:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      {/* Your main content goes here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
